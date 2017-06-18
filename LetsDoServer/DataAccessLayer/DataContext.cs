0e21c2ca96a8c1e7758cba69b3532f48c17cad07
Ryan Malencia <ryan.malencia@gmail.com>
2017-06-18 12:11:58 -0400

    InitialCommit

---
+++ LetsDoServer/DataAccessLayer/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+namespace LetsDoServer.DataAccessLayer
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DataContext")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ LetsDoServer/DataAccessLayer/DataInitializer.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace LetsDoServer.DataAccessLayer
+{
+    public class DataInitializer : DropCreateDatabaseIfModelChanges<DataContext>
+    {
+        protected override void Seed(DataContext context)
+        {
+            context.SaveChanges();
+        }
+    }
+}
